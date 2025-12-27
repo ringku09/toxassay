4843280cfe26704e10d2043306e59b18c5e21b3a
ringku09 <ringku_740@yahoo.com>
2025-12-27 14:34:30 +0600

    update export

--- R/reexport.R
+++ R/reexport.R
@@ -1,394 +1,317 @@
-#-------------------------------  Base packages ---------------------
-#' @importFrom stats na.omit
+#' Matrix class imports
+#'
+#' @importClassesFrom Matrix dgCMatrix
+NULL
+
+#' Re-export selected functions from stats
+#'
+#' @importFrom stats na.omit confint pf median p.adjust
 #' @export
 stats::na.omit
-
-#' @importFrom stats confint
 #' @export
 stats::confint
-
-#' @importFrom stats pf
 #' @export
 stats::pf
+#' @export
+stats::median
+#' @export
+stats::p.adjust
 
+#' Re-export selected functions from methods
+#'
 #' @importFrom methods as
 #' @export
 methods::as
 
+#' Re-export selected functions from scales
+#'
 #' @importFrom scales rescale
 #' @export
 scales::rescale
 
-#' @importFrom utils unzip
+#' Re-export selected functions from utils
+#'
+#' @importFrom utils unzip read.table txtProgressBar setTxtProgressBar download.file packageVersion
 #' @export
 utils::unzip
-
-#' @importFrom utils unzip
-#' @export
-utils::unzip
-
-#' @importFrom utils read.table
 #' @export
 utils::read.table
-
-#' @importFrom utils txtProgressBar
 #' @export
 utils::txtProgressBar
-
-#' @importFrom utils setTxtProgressBar
 #' @export
 utils::setTxtProgressBar
-
-#' @importFrom utils download.file
 #' @export
 utils::download.file
-
-#' @importFrom utils unzip
-#' @export
-utils::unzip
-
-#' @importFrom utils packageVersion
 #' @export
 utils::packageVersion
 
+#' Re-export selected functions from tools
+#'
 #' @importFrom tools file_ext
 #' @export
 tools::file_ext
 
-#' @importFrom stats median
+#--------------------------- Biological databases -------------------
+
+#' Re-export selected functions/objects from Bioconductor annotation packages
+#'
+#' @importFrom AnnotationDbi select
+#' @importFrom rat2302.db rat2302.db
 #' @export
-stats::median
+AnnotationDbi::select
+#' @export
+rat2302.db::rat2302.db
 
-#-------------------------------  Biological database ---------------------
-  #' @importFrom AnnotationDbi select
-  #' @export
-  AnnotationDbi::select
+#------------------------------- magrittr ---------------------------
 
-  #' @importFrom rat2302.db rat2302.db
-  #' @export
-  rat2302.db::rat2302.db
-
-
+#' Re-export the pipe operator
+#'
 #' @importFrom magrittr %>%
 #' @export
- magrittr::`%>%`
+magrittr::`%>%`
 
- #' @importFrom affy ReadAffy
- #' @export
- affy::ReadAffy
+#------------------------------- affy / Biobase ---------------------
 
- #' @importFrom affy mas5
- #' @export
- affy::mas5
-
- #' @importFrom affy rma
- #' @export
- affy::rma
-
- #' @importFrom Biobase exprs
- #' @export
- Biobase::exprs
-
- #----------------------------- import for `dplyr`--------------------
- #' @importFrom dplyr across
- #' @export
- dplyr::across
-
- #' @importFrom dplyr pull
- #' @export
- dplyr::pull
-
- #' @importFrom dplyr distinct
- #' @export
- dplyr::distinct
-
- #' @importFrom dplyr ungroup
- #' @export
- dplyr::ungroup
-
- #' @importFrom dplyr arrange
- #' @export
- dplyr::arrange
-
- #' @importFrom dplyr group_by
- #' @export
- dplyr::group_by
-
- #' @importFrom dplyr select
- #' @export
- dplyr::select
-
- #' @importFrom dplyr select
- #' @export
- dplyr::arrange
-
- #' @importFrom dplyr filter
- #' @export
- dplyr::filter
-
- #' @importFrom dplyr rename
- #' @export
- dplyr::rename
-
- #' @importFrom dplyr rename_with
- #' @export
- dplyr::rename_with
-
- #' @importFrom dplyr left_join
- #' @export
- dplyr::left_join
-
- #' @importFrom  dplyr bind_rows
- #' @export
- dplyr::bind_rows
-
-
-
- #------------------------------------- Tidyselect ---------------
-
- #' @importFrom tidyselect everything
- #' @export
- tidyselect::everything
-
- #' @importFrom dplyr mutate_at
- #' @export
- dplyr::mutate_at
-
- #' @importFrom tibble as_tibble
- #' @export
- tibble::as_tibble
-
- #' @importFrom readr write_csv
- #' @export
- readr::write_csv
-
- #------------------------------------ parralel computing --------------------
- #' @importFrom parallel detectCores
- #' @export
- parallel::detectCores
-
- #' @importFrom parallel splitIndices
- #' @export
- parallel::splitIndices
-
- #' @importFrom parallel makeCluster
- #' @export
- parallel::makeCluster
-
- #' @importFrom parallel clusterExport
- #' @export
- parallel::clusterExport
-
- #' @importFrom parallel clusterCall
- #' @export
- parallel::clusterCall
-
- #' @importFrom parallel stopCluster
- #' @export
- parallel::stopCluster
-
- #' @importFrom doParallel registerDoParallel
- #' @export
- doParallel::registerDoParallel
-
- #' @importFrom foreach getDoParName
- #' @export
- foreach::getDoParName
-
- #' @importFrom foreach getDoParWorkers
- #' @export
- foreach::getDoParWorkers
-
- #' @importFrom foreach registerDoSEQ
- #' @export
- foreach::registerDoSEQ
-
- #' @importFrom foreach foreach
- #' @export
- foreach::foreach
-
- #' @importFrom foreach `%dopar%`
- #' @export
- foreach::`%dopar%`
-
- #' @importFrom foreach `%do%`
- #' @export
- foreach::`%do%`
-
-
-
- #----------------------------- import for `rlang`--------------------
- #' @importFrom rlang is_empty
- #' @export
- rlang::is_empty
-
- #' @importFrom rlang is_missing
- #' @export
- rlang::is_missing
-
- #' @importFrom rlang caller_env
- #' @export
- rlang::caller_env
-
-
- #' @importFrom rlang .data
- #' @export
- rlang::.data
-
- #' @importFrom rlang englue
- #' @export
- rlang::englue
-
- #' @importFrom rlang list2
- #' @export
- rlang::list2
-
- #' @importFrom rlang is_bare_list
- #' @export
- rlang::is_bare_list
-
- #' @importFrom rlang inherits_any
- #' @export
- rlang::inherits_any
-
- #' @importFrom rlang is_missing
- #' @export
- rlang::is_missing
-
- #' @importFrom rlang missing_arg
- #' @export
- rlang::missing_arg
-
- #' @importFrom rlang parse_expr
- #' @export
- rlang::parse_expr
- #----------------------------- import for `cli`--------------------
- #' @importFrom cli cli_alert_warning
- #' @export
- cli::cli_alert_warning
-
- cli_abort
- #' @importFrom cli cli_abort
- #' @export
- cli::cli_abort
-
- #' @importFrom cli style_bold
- #' @export
- cli::style_bold
-
- #' @importFrom cli cli_ul
- #' @export
- cli::cli_ul
-
- #' @importFrom cli bg_magenta
- #' @export
- cli::bg_magenta
-
- #' @importFrom cli bg_red
- #' @export
- cli::bg_red
-
- #' @importFrom cli bg_red
- #' @export
- cli::bg_red
-
- #' @importFrom cli col_red
- #' @export
- cli::col_red
-
- #' @importFrom cli col_green
- #' @export
- cli::col_green
-
- #' @importFrom cli cli_alert_info
- #' @export
- cli::cli_alert_info
-
- #' @importFrom cli col_br_red
- #' @export
- cli::col_br_red
-
- #' @importFrom cli cli_alert_success
- #' @export
- cli::cli_alert_success
-
- #----------------------------- import for `glue`--------------------
- #' @importFrom glue backtick
- #' @export
- glue::backtick
-
- #' @importFrom glue glue
- #' @export
- glue::glue
-
- #' @importFrom glue glue_collapse
- #' @export
- glue::glue_collapse
-
- #----------------------------- import for `tidyr`--------------------
- #' @importFrom tidyr nest
- #' @export
- tidyr::nest
-
- #' @importFrom tidyr pivot_wider
- #' @export
- tidyr::pivot_wider
-
- #----------------------------- import for `purrr`--------------------
-
- #' @importFrom  purrr map
- #' @export
- purrr::map
-
- #' @importFrom  purrr imap
- #' @export
- purrr::imap
-
- #----------------------------- import for `readr`--------------------
-
- #' @importFrom  readr write_tsv
- #' @export
- readr::write_tsv
-
- #' @importFrom  readr write_csv
- #' @export
- readr::write_csv
-
- #----------------------------- import for `tibble`--------------------
- #' @importFrom tibble tibble
- #' @export
- tibble::tibble
-
- #' @importFrom tibble as_tibble
- #' @export
- tibble::as_tibble
-
- #---------------------------------- httr -----------------------
-
- #' @importFrom httr GET
- #' @export
- httr::GET
-
- #' @importFrom httr write_disk
- #' @export
- httr::write_disk
-
- #' @importFrom httr progress
- #' @export
- httr::progress
-
- #' @importFrom  httr write_disk
- #' @export
- httr::write_disk
-
-
- #############################################
-
-#
-#  #' @import arules
-#  #' @export
-
-#' @import STRINGdb
+#' Re-export selected functions from affy
+#'
+#' @importFrom affy ReadAffy mas5 rma
 #' @export
+affy::ReadAffy
+#' @export
+affy::mas5
+#' @export
+affy::rma
 
+#' Re-export selected functions from Biobase
+#'
+#' @importFrom Biobase exprs
+#' @export
+Biobase::exprs
 
+#------------------------------- dplyr ------------------------------
+
+#' Re-export selected functions from dplyr
+#'
+#' @importFrom dplyr across pull distinct ungroup arrange group_by select filter rename rename_with left_join bind_rows mutate_at
+#' @export
+dplyr::across
+#' @export
+dplyr::pull
+#' @export
+dplyr::distinct
+#' @export
+dplyr::ungroup
+#' @export
+dplyr::arrange
+#' @export
+dplyr::group_by
+#' @export
+dplyr::select
+#' @export
+dplyr::filter
+#' @export
+dplyr::rename
+#' @export
+dplyr::rename_with
+#' @export
+dplyr::left_join
+#' @export
+dplyr::bind_rows
+#' @export
+dplyr::mutate_at
+
+#------------------------------ tidyselect --------------------------
+
+#' Re-export selected functions from tidyselect
+#'
+#' @importFrom tidyselect everything
+#' @export
+tidyselect::everything
+
+#------------------------------- tidyr ------------------------------
+
+#' Re-export selected functions from tidyr
+#'
+#' @importFrom tidyr nest pivot_wider
+#' @export
+tidyr::nest
+#' @export
+tidyr::pivot_wider
+
+#------------------------------- purrr ------------------------------
+
+#' Re-export selected functions from purrr
+#'
+#' @importFrom purrr map imap
+#' @export
+purrr::map
+#' @export
+purrr::imap
+
+#------------------------------- readr ------------------------------
+
+#' Re-export selected functions from readr
+#'
+#' @importFrom readr write_csv write_tsv
+#' @export
+readr::write_csv
+#' @export
+readr::write_tsv
+
+#------------------------------- tibble -----------------------------
+
+#' Re-export selected functions from tibble
+#'
+#' @importFrom tibble tibble as_tibble
+#' @export
+tibble::tibble
+#' @export
+tibble::as_tibble
+
+#------------------------------ parallel ----------------------------
+
+#' Re-export selected functions from parallel
+#'
+#' @importFrom parallel detectCores splitIndices makeCluster clusterExport clusterCall stopCluster
+#' @export
+parallel::detectCores
+#' @export
+parallel::splitIndices
+#' @export
+parallel::makeCluster
+#' @export
+parallel::clusterExport
+#' @export
+parallel::clusterCall
+#' @export
+parallel::stopCluster
+
+#----------------------------- doParallel ---------------------------
+
+#' Re-export selected functions from doParallel
+#'
+#' @importFrom doParallel registerDoParallel
+#' @export
+doParallel::registerDoParallel
+
+#------------------------------- foreach ----------------------------
+
+#' Re-export selected functions from foreach
+#'
+#' @importFrom foreach getDoParName getDoParWorkers registerDoSEQ foreach %dopar% %do%
+#' @export
+foreach::getDoParName
+#' @export
+foreach::getDoParWorkers
+#' @export
+foreach::registerDoSEQ
+#' @export
+foreach::foreach
+#' @export
+foreach::`%dopar%`
+#' @export
+foreach::`%do%`
+
+#------------------------------- rlang ------------------------------
+
+#' Re-export selected functions from rlang
+#'
+#' @importFrom rlang is_empty is_missing caller_env .data englue list2 is_bare_list inherits_any missing_arg parse_expr
+#' @export
+rlang::is_empty
+#' @export
+rlang::is_missing
+#' @export
+rlang::caller_env
+#' @export
+rlang::.data
+#' @export
+rlang::englue
+#' @export
+rlang::list2
+#' @export
+rlang::is_bare_list
+#' @export
+rlang::inherits_any
+#' @export
+rlang::missing_arg
+#' @export
+rlang::parse_expr
+
+#-------------------------------- cli -------------------------------
+
+#' Re-export selected functions from cli
+#'
+#' @importFrom cli cli_abort cli_alert_warning cli_alert_info cli_alert_success style_bold cli_ul
+#' @importFrom cli bg_magenta bg_red col_red col_green col_br_red
+#' @export
+cli::cli_abort
+#' @export
+cli::cli_alert_warning
+#' @export
+cli::cli_alert_info
+#' @export
+cli::cli_alert_success
+#' @export
+cli::style_bold
+#' @export
+cli::cli_ul
+#' @export
+cli::bg_magenta
+#' @export
+cli::bg_red
+#' @export
+cli::col_red
+#' @export
+cli::col_green
+#' @export
+cli::col_br_red
+
+#-------------------------------- glue ------------------------------
+
+#' Re-export selected functions from glue
+#'
+#' @importFrom glue backtick glue glue_collapse
+#' @export
+glue::backtick
+#' @export
+glue::glue
+#' @export
+glue::glue_collapse
+
+#-------------------------------- httr ------------------------------
+
+#' Re-export selected functions from httr
+#'
+#' @importFrom httr GET write_disk progress
+#' @export
+httr::GET
+#' @export
+httr::write_disk
+#' @export
+httr::progress
+
+#-------------------------------- arules ------------------------------
+
+#' Re-export selected functions from arules
+#'
+#' @importFrom arules intersect apriori subset %in% quality interestMeasure
+#' @export
+arules::intersect
+#' @export
+arules::apriori
+#' @export
+arules::subset
+#' @export
+arules::`%in%`
+#' @export
+arules::quality
+#' @export
+arules::interestMeasure
+
+#------------------------------ Package imports ---------------------
+
+#' Import STRINGdb
+#'
+#' @import STRINGdb
+NULL
